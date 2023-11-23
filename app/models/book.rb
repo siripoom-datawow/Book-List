0fe3e4aaddaceed11200052cd0c393cc4b180f34
Siripoom Luengsrisakul <siripoom.l@datawow.io>
2023-11-23 17:25:02 +0700

    feat: add model validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :release, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ApplicationRecord
   belongs_to :book
+  validates :comment, presence: true , length: { minimum: 10 }
 end
