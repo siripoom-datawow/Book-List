75e1082b5f368c7e92db59eae4589bb7687793f6
Siripoom Luengsrisakul <siripoom.l@datawow.io>
2023-11-28 00:08:35 +0700

    test: Review Controller test case

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -2,12 +2,41 @@
 
 require 'rails_helper'
 
-RSpec.describe BooksController, type: :controller do
+RSpec.describe ReviewsController, type: :controller do
   describe 'GET #index' do
     subject { get :index }
-    it 'return all books' do
+    it 'return all reviews' do
       expect(response).to be_successful
     end
   end
 
+  describe 'POST #create' do
+    subject { post :create, params: }
+    let(:book) { create(:book) }
+
+    context 'when validation success' do
+      let(:params) { { book_id: book.id, review: attributes_for(:review) } }
+      it 'creates a new revirw' do
+        expect { subject }.to change(Review, :count).by(1)
+        expect(subject).to redirect_to(Book.find(book.id))
+      end
+    end
+
+    context 'when validation fail' do
+      let(:params) { { book_id: book.id, review: attributes_for(:review, comment: '') } }
+      it 'raise RecordInvalid  if no review detail' do
+        expect { subject }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+
+    describe 'DELETE #destroy' do
+      subject { delete :destroy, params: { book_id: book.id, id: review.id } }
+      let(:book) { create(:book) }
+      let!(:review) { create(:review, book_id: book.id) }
+      it 'Can delete review' do
+        expect { subject }.to change(Review, :count).by(-1)
+        expect(subject).to redirect_to(Book.find(book.id))
+      end
+    end
+  end
 end
